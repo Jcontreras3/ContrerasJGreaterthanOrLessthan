165a4a1624366602100d37af67c3311f8f671405
Jovann Contreras <jcontreras@codestack.co>
2022-10-20 14:55:17 -0700

    comment

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿
+﻿// Jovann Contreras
+// Date 10/18/2022
+// Checking if numbers are greater than, less than or equal too with data validation.
+
+
 Console.Clear();
 string playAgain = "yes";
 while (playAgain == "yes")
